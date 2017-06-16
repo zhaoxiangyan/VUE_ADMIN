bf0ef0e884ea4080a48268dccfd05dac84cbe5e4
Andy <987670346@qq.com>
2017-06-16 17:56:46 +0800

    6.16下班提交

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="header">
-        <div class="logo">后台管理系统</div>
-        <div class="user-info">
-            <el-dropdown trigger="click" @command="handleCommand">
-                <span class="el-dropdown-link">
-
-                    {{username}}
-                </span>
-                <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item command="loginout">退出</el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
+        <div class="header">
+            <div class="left">
+                GQFX-CRM
+            </div>
+            <div class="center">
+                <i class="el-icon-d-arrow-left"></i>
+            </div>
+            <div class="right">
+                 <ul></ul>
+            </div>
         </div>
-    </div>
 </template>
 <script>
     export default {
+      name: 'header',  
       data () {
         return {
           name: 'linxin'
@@ -50,35 +48,5 @@
         line-height: 70px;
         color: #fff;
         background:#242f42;
-    }
-    .header .logo{
-        float: left;
-        width:250px;
-        text-align: center;
-    }
-    .user-info {
-        float: right;
-        padding-right: 50px;
-        font-size: 16px;
-        color: #fff;
-    }
-    .user-info .el-dropdown-link{
-        position: relative;
-        display: inline-block;
-        padding-left: 50px;
-        color: #fff;
-        cursor: pointer;
-        vertical-align: middle;
-    }
-    .user-info .user-logo{
-        position: absolute;
-        left:0;
-        top:15px;
-        width:40px;
-        height:40px;
-        border-radius: 50%;
-    }
-    .el-dropdown-menu__item{
-        text-align: center;
     }
 </style>
