dce0546db2c01be809bd66acb342b57a915de9bd
Andy <987670346@qq.com>
2017-06-19 14:22:29 +0800

    使用vuex之前备份

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,10 +4,17 @@
                 GQFX-CRM
             </div>
             <div class="center">
-                <i class="el-icon-d-arrow-left"></i>
+                <i class="el-icon-d-arrow-left" @click.prevent="collapse"></i>
             </div>
             <div class="right">
-                 <ul></ul>
+                  <el-dropdown trigger="hover">
+                   <span class="el-dropdown-link">andy<img src="../../assets/user_photo.jpg"></span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>我的消息</el-dropdown-item>
+                      <el-dropdown-item>设置</el-dropdown-item>
+                      <el-dropdown-item divided>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                 </el-dropdown>  
             </div>
         </div>
 </template>
@@ -16,24 +23,14 @@
       name: 'header',  
       data () {
         return {
-          name: 'linxin'
-        }
-      },
-      computed: {
-        username () {
-          let username = localStorage.getItem('ms_username')
-          // return username ? username : this.name
-          if (!username) {
-            username = this.name
-          }
+          name: 'linxin',
+          collapsed:false
         }
       },
       methods: {
-        handleCommand (command) {
-          if (command === 'loginout') {
-            localStorage.removeItem('ms_username')
-            this.$router.push('/login')
-          }
+        //折叠导航栏
+        collapse:function(){
+          this.collapsed=!this.collapsed;
         }
       }
     }
@@ -48,5 +45,64 @@
         line-height: 70px;
         color: #fff;
         background:#242f42;
+    }
+    .header .left{
+        box-sizing:border-box;
+        width:251px;
+        height:70px;
+        padding-left:20px;
+        padding-right:20px;
+        border-color:rgba(238,241,146,0.3);
+        border-width:1px;
+        border-right-style:solid;
+        float:left;
+    }
+    .header .center{
+        padding:0 23px;
+        width:22px;
+        height:70px;
+        line-height:70px;
+        cursor:pointer;
+        float:left;
+    }
+    .header .right{
+      float:right;
+      padding-right:35px;
+      text-align:right;
+      position:relative;
+      height:70px;
+    }
+    .right span{
+      cursor:pointer;
+      color:#fff;
+      font-size:18px;
+    }
+    .right span img{
+      width:50px;
+      height:50px;
+      border-radius:25px;
+      margin:10px 0 10px 10px;
+      float:right;
+    }
+    .right ul{
+      margin: 5px 0;
+      border: 1px solid #d1dbe5;
+      box-shadow: 0 2px 4px rgba(0,0,0,.12), 0 0 6px rgba(0,0,0,.12);
+      padding: 6px 0;
+      z-index: 10;
+      position: absolute;
+      top: 0;
+      left: 0;
+      min-width: 100px;
+      background:#fff;
+    }
+    .right ul li{
+      color:#000;
+      font-size:14px;
+      list-style: none;
+      line-height: 36px;
+      padding: 0 10px;
+      margin: 0;
+      cursor: pointer;
     }
 </style>
