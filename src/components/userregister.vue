9bdbf13dae79776d940c5e8e77195cff29f8b506
Andy <987670346@qq.com>
2017-06-30 17:45:30 +0800

    6.30用户注册页面初步实现userregister.vue

--- src/components/userregister.vue
+++ src/components/userregister.vue
@@ -20,7 +20,7 @@
                </li>
            </ul>
            <el-dropdown class="language fr" trigger="hover">
-                   <span class="el-dropdown-link"><img src="../assets/China.png">{{$t("Item[0].menu")}}</span>
+                    <span class="el-dropdown-link"><img src="../assets/China.png">{{$t("Item[0].menu")}}</span>
                     <el-dropdown-menu class="language_box"  slot="dropdown">
                       <el-dropdown-item @click.native="ll('cn')"><img src="../assets/China.png">简体中文</el-dropdown-item>
                       <el-dropdown-item @click.native="ll('ja')"><img src="../assets/Japan.png">日语</el-dropdown-item>
@@ -30,9 +30,144 @@
        </div>
    </div>
    <!--左边折叠导航栏-->
-   <div class="left_collapse"></div>
+   <div class="left_collapse">
+        <ul>
+           <li>导航</li>
+        </ul>
+   </div>
    <!--注册表单内容块-->
-   <div class="content"></div>
+   <div class="content">
+        <div class="content_title">
+           <h1>注册真实账号</h1>
+           <el-breadcrumb separator="/">
+                <el-breadcrumb-item>当前位置</el-breadcrumb-item>
+                <el-breadcrumb-item>注册真实账号</el-breadcrumb-item>
+           </el-breadcrumb>
+        </div>
+        <div class="main">
+          <form>
+             <h3 class="form_title">注册真实账号</h3>
+             <div class="form_body">
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*姓名:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="username" placeholder="username"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*推荐人编号:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="parentid" placeholder="parentid"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*身份证号码:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="identifier" placeholder="identifier"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*手机:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="mobile" placeholder="mobile"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*邮箱:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="email" placeholder="email"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*国家:</el-col>
+                    <el-col :sm="12" :lg="12">
+                        <el-select v-model="value" placeholder="country" style="width:100%;" name="country">
+                            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*省/州:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="province" placeholder="province"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*市:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="city" placeholder="city"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*县:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="town" placeholder="town"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*详细地址:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input name="address" placeholder="address"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*账号密码:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input type="password" name="password" placeholder="password"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*确认账号密码:</el-col>
+                    <el-col :sm="12" :lg="12">
+                       <el-input type="password" name="repassword" placeholder="repassword"></el-input>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*上传身份证正面:</el-col>
+                    <el-col :sm="12" :lg="12">
+                        <el-upload 
+                        class="upload-demo fl"
+                        drag name="image1"
+                        action="https://jsonplaceholder.typicode.com/posts/">
+                        <div class="el-upload__text">将图片拖到此处，或<em>点击上传</em></div>
+                        </el-upload>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*上传身份证反面:</el-col>
+                    <el-col :sm="12" :lg="12">
+                        <el-upload 
+                        class="upload-demo fl"
+                        drag name="image2"
+                        action="https://jsonplaceholder.typicode.com/posts/">
+                        <div class="el-upload__text">将图片拖到此处，或<em>点击上传</em></div>
+                        </el-upload>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6">*</el-col>
+                    <el-col :sm="12" :lg="12" style="text-align:left;">
+                    注意事项：上传的单张图片大小不要超过2M;否则影响开户！
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6"></el-col>
+                    <el-col :sm="12" :lg="12" style="text-align:left;">
+                     <el-checkbox v-model="checked" name="agree">我已阅读，了解并接受</el-checkbox>
+                     <a href="javascript:void(0);" style="color:red;font-size:14px;">客户协议</a>
+                    </el-col>
+                 </el-row>
+                 <el-row :gutter="15">
+                    <el-col class="input_title" :sm="12" :lg="6"></el-col>
+                    <el-col :sm="12" :lg="12" style="text-align:left;">
+                       <el-button type="primary" style="width:130px;">注册</el-button>
+                    </el-col>
+                 </el-row>
+             </div>
+          </form>
+        </div>
+   </div>
 </div>
 </template>
 
@@ -41,9 +176,50 @@
     import i18n from '../vueI18n/i18n.js'
 export default {
     name: 'userregister',
+    data () {
+       return {
+          options: [{
+          value: '选项1',
+          label: 'China'
+        }, {
+          value: '选项2',
+          label: 'Japan'
+        }, {
+          value: '选项3',
+          label: 'English'
+        }, {
+          value: '选项4',
+          label: 'America'
+        }, {
+          value: '选项5',
+          label: 'New Zealand'
+        }],
+        value: '',
+        // 身份证上传
+        imageUrl: '',
+        // 勾选框数据
+        checked: true
+      } 
+    },
     methods: {
         ll: function(n){
           i18n.locale = n;
+        },
+        // 身份证上传
+        handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPG) {
+            this.$message.error('上传头像图片只能是 JPG 格式!');
+            }
+            if (!isLt2M) {
+            this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
         }
     }
 }    
@@ -64,6 +240,7 @@ export default {
     width:100%;
     height:100%;
     overflow-y:scroll;
+    background:#f1f4f5;
     .nav{
         position: fixed;
         right: 0;
@@ -73,6 +250,8 @@ export default {
         width: 100%;
         background-color: #3a3f51;
         height:66px;
+        -webkit-box-shadow: 0 2px 4px rgba(0,0,0,.08);
+        box-shadow: 0 2px 4px rgba(0,0,0,.08);
         .nav_header{
             float: left;
             width: 260px;
@@ -87,11 +266,11 @@ export default {
             padding-right: 15px;
             padding-left: 15px;
             margin-right: auto;
-            margin-left: auto;
             background-color: #fff;
             position:relative;
             margin-left: 260px;
             height:66px;
+            min-width:360px;
             ul.nav_toolbar{
                 float:left;
                 li{float:left;display:block;}
@@ -103,7 +282,7 @@ export default {
                     }
                     span{
                         position:absolute;
-                        // display:none;
+                        display:none;
                         box-sizing:border-box;
                         -webkit-box-sizing:border-box;
                         -moz-box-sizing:border-box;
@@ -137,9 +316,12 @@ export default {
             }
             div.language{
                 height:66px;
+                min-width:120px;
                 span{
                     display:inline-block;
                     margin-top:15px;
+                    font-size:17px;
+                    cursor:pointer;
                     img{
                         height:35px;
                         margin-right:5px;
@@ -148,6 +330,93 @@ export default {
             }
         }
     }
+    .left_collapse{
+        z-index: 1400;
+        position: fixed;
+        top: 66px;
+        height: 100%;
+        // height: -webkit-calc(100% - 66px);
+        // height: calc(100% - 66px);
+        background: #3a3f51;
+        color: rgba(163,175,183,.9);
+        font-family: 'microsoft yahei', Roboto, sans-serif;
+        -webkit-box-shadow: 0 2px 4px rgba(0,0,0,.08);
+        box-shadow: 0 2px 4px rgba(0,0,0,.08);
+        width:260px;
+        ul{
+            li{
+                -webkit-transition: all .25s, font .1s .15s, color .1s .15s;
+                -o-transition: all .25s, font .1s .15s, color .1s .15s;
+                transition: all .25s, font .1s .15s, color .1s .15s;
+                font-size: 15px;
+                padding: 0 24px;
+                color: #76838f;
+                line-height: 38px;
+                text-transform: uppercase;
+                margin-top: 20px;
+                text-align:left;
+            }
+        }
+    }
+    .content{
+        background:#f1f4f5;
+        // background:hotpink;
+        position: relative;
+        // min-height: -webkit-calc(100% - 44px);
+        // min-height: calc(100% - 44px);
+        margin-left:260px;
+        margin-top:66px;
+        .content_title{
+            margin: 44px 0 22px;  
+            padding: 30px 30px;
+            margin-top: 0;
+            margin-bottom: 0;
+            background: 0 0;
+            position: relative;
+            border-bottom: none;
+            h1{font-size:26px;text-shadow: rgba(0,0,0,.15) 0 0 1px;
+            color: #37474f;text-align:left;font-weight:400;margin-bottom:5px;}
+        }
+        .main{
+            // padding:15px;
+            // padding-top:0;
+            background:#fff;
+            margin:15px;
+            border-radius:4px;
+            margin-bottom:100px;
+            padding-bottom:30px;
+            form{
+                h3{
+                   padding: 20px 30px;
+                   font-size: 18px;
+                   color: #37474f; 
+                   font-weight:400;
+                   text-align:left;
+                }
+                .form_body{
+                    .el-row{
+                        margin-top:10px;
+                        .input_title{
+                            text-align:right;
+                            padding:6px;
+                            font-size:16px;
+                            color:#48576a;
+                        }
+                        .el-col{
+                            input{width:100%;}
+                        }
+                    }
+                }
+            }
+        }
+    }
 } 
-   
+// 语言选择样式
+.language_box li img{height:35px;}  
+// element面包屑样式
+// span.el-breadcrumb__item{color: #97a8be!important;}
+.el-breadcrumb{font-size:15px;}
+// 上传图片
+// .el-upload{width:100%;height:100%;}
+// .el-upload-dragger{width:500px;height:34px;}
 </style>
